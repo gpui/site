ce68cafc329ad2cb8e246e2ace50cb5a4a64672d
Nate Butler <iamnbutler@gmail.com>
2023-10-30 22:09:11 -0400

    Update site design with new UI framework and add navigation component

---
+++ src/app/nav.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import clsx from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type NavItemProps = {
+  href: string;
+  title: string;
+};
+
+export const NavItem = ({ href, title }: NavItemProps) => {
+  const currentPath = usePathname();
+
+  return (
+    <Link
+      href={href}
+      className={clsx(
+        "font-medium text-lg text-stone-200",
+        currentPath === href ? "opacity-100" : "opacity-60 hover:opacity-80"
+      )}
+    >
+      {title}
+    </Link>
+  );
+};
+
+export const Nav = () => {
+  return (
+    <nav className="flex gap-8 items-center font-medium text-lg">
+      <NavItem href="/" title="gpui" />
+      <NavItem href="/docs" title="docs" />
+      <NavItem href="https://github.com/gpui" title="github" />
+    </nav>
+  );
+};
