945a38b8b6d8c8d535e1e39cf13b2ff2f7c581ea
Marshall Bowers <elliott.codes@gmail.com>
2024-01-18 12:15:04 -0500

    Fix typo in environment variable name

--- src/lib/octokit.ts
+++ src/lib/octokit.ts
@@ -1,5 +1,8 @@
 import { Octokit } from "octokit";
 
 export const octokit = new Octokit({
-  auth: process.env.GIHUB_PERSONAL_ACCESS_TOKEN,
+  auth:
+    process.env.GITHUB_PERSONAL_ACCESS_TOKEN ||
+    // Typoed version. We can remove if no one is relying on it.
+    process.env.GIHUB_PERSONAL_ACCESS_TOKEN,
 });
