9331af9886b05498773318d9c98ac22b98d56cbb
Marshall Bowers <elliott.codes@gmail.com>
2024-01-23 18:54:58 -0500

    Format everything with Prettier

--- src/font/index.ts
+++ src/font/index.ts
@@ -1,16 +1,16 @@
 import localFont from "next/font/local";
 
 export const agrandir_grand = localFont({
-    src: [
-        {
-            path: "./PPAgrandir-GrandHeavy.otf",
-            weight: "800",
-            style: "heavy",
-        },
-        {
-            path: "./PPAgrandir-GrandLight.otf",
-            weight: "300",
-            style: "light",
-        },
-    ],
+  src: [
+    {
+      path: "./PPAgrandir-GrandHeavy.otf",
+      weight: "800",
+      style: "heavy",
+    },
+    {
+      path: "./PPAgrandir-GrandLight.otf",
+      weight: "300",
+      style: "light",
+    },
+  ],
 });
